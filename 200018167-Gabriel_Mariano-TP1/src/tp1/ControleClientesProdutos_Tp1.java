ae71070e877fa817af68efa76757556b35d63b58
Gabriel Mariano <mariano.gabriel2q@gmail.com>
2021-07-26 12:15:37 -0300

    Atualização: Aplicando os Vetores

--- 200018167-Gabriel_Mariano-TP1/src/tp1/ControleClientesProdutos_Tp1.java
+++ 200018167-Gabriel_Mariano-TP1/src/tp1/ControleClientesProdutos_Tp1.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class ControleClientesProdutos_Tp1 {
 	public static int TAMANHO_VETOR = 50;
 	public static int numClientes = 0;
+	public static int numProdutos = 0;
 	
 	public static void main(String[] args) { // Método Principal
 		int opcMenu;
@@ -39,11 +40,11 @@ public class ControleClientesProdutos_Tp1 {
 					break;
 				case 3: // Cadastro de novo produto
 					criarLinha(60);
-					//cadastrarProdutos(nomeProduto, descricao, valorCompra, porcentLucro, qtdEstoque);
+					cadastrarProdutos(nomeProduto, descricao, valorCompra, porcentLucro, qtdEstoque);
 					break;
 				case 4: // Busca por produto
 					criarLinha(60);
-					//buscarProdutos(nomeProduto, descricao, valorCompra, porcentLucro, qtdEstoque);
+					buscarProdutos(nomeProduto, descricao, valorCompra, porcentLucro, qtdEstoque);
 					break;
 				case 5: // Cadastro de venda
 					break;
@@ -53,8 +54,7 @@ public class ControleClientesProdutos_Tp1 {
 					criarLinha(60);
 					System.out.println("\nEncerrando! Obrigado por usar o programa!");
 					break;
-				default: // O default está sendo usado apenas para evitar possíveis problemas, pois os
-						 // valores já são verificados no próprio menu!
+				default: // O default está sendo usado apenas para evitar possíveis problemas, pois os valores já são verificados no próprio menu!
 					criarLinha(60);
 					System.out.println("\nERRO!");
 					criarLinha(60);
@@ -117,14 +117,14 @@ public class ControleClientesProdutos_Tp1 {
 		Scanner ler = new Scanner(System.in);
 		
 		System.out.println("\n~ CADASTRAR CLIENTES ~");
-		do { // O usuário deverá digitar uma quant. de clientes a serem cadastrados maior ou igual a 1
+		do { // O usuário deverá digitar uma quant. de clientes a serem cadastrados maior ou igual a 1 e menor que o tamanho máximo dos vetores (ver TAMANHO_VETOR)
 			System.out.print("Digite a quantidade de clientes a serem cadastrados: ");
 			qtdCadastrar = ler.nextInt();	
 			
-			if(qtdCadastrar < 1) { // Apresenta mensagem para informar o erro ao usuário
-				System.out.println("O valor inserido deve ser maior que zero!");
+			if(qtdCadastrar < 1 || qtdCadastrar > TAMANHO_VETOR) { // Apresenta mensagem para informar o erro ao usuário
+				System.out.println("O valor inserido deve ser maior que zero e menor que " + (TAMANHO_VETOR+1) + "!\n");
 			}
-		}while(qtdCadastrar < 1); //Se repete até a quant. ser maior ou igual a 1
+		}while(qtdCadastrar < 1 || qtdCadastrar > TAMANHO_VETOR); //Se repete até a quant. ser maior ou igual a 1
 		
 		ler.nextLine(); // Limpando o Buffer do Teclado
 		
@@ -146,7 +146,7 @@ public class ControleClientesProdutos_Tp1 {
 	
 	public static void buscarClientes(String nome[], String endereco[], String telefone[]) {
 		String nomeBuscar;
-		int posicaoCliente = 0;
+		int indexCliente = 0;
 		boolean clienteExiste = false;
 		char desejaAlterar;		
 		Scanner ler = new Scanner(System.in);
@@ -157,17 +157,16 @@ public class ControleClientesProdutos_Tp1 {
 		
 		for(int i = 0; i < numClientes; i++) {
 			if(nome[i].equals(nomeBuscar)) {
-				System.out.println("TRUE");
 				clienteExiste = true;
-				posicaoCliente = i;
+				indexCliente = i;
 			}
 		}
 		
 		if(clienteExiste) {
 			System.out.println("\nDADOS DO CLIENTE: ");
-			System.out.println("Nome: " + nome[posicaoCliente]);
-			System.out.println("Endereço: " + endereco[posicaoCliente]);
-			System.out.println("Telefone: " + telefone[posicaoCliente]);
+			System.out.println("Nome: " + nome[indexCliente]);
+			System.out.println("Endereço: " + endereco[indexCliente]);
+			System.out.println("Telefone: " + telefone[indexCliente]);
 			
 			do { // Verificando se o usuário deseja alterar os dados do cliente em questão 
 				System.out.print("\nDeseja alterar os dados do(a) cliente? ('S' ou 'N'): ");
@@ -175,9 +174,7 @@ public class ControleClientesProdutos_Tp1 {
 				ler.nextLine(); //Limpando Buffer
 				
 				if (desejaAlterar != 's' && desejaAlterar != 'S' && desejaAlterar != 'n' && desejaAlterar != 'N') {
-					System.out.println("Caractere inválido!"); // Se o usuário digitar um caractere diferente do
-															   // esperado, será apresentado o erro e ele deverá
-															   // digitar um caractere válido para encerrar o loop
+					System.out.println("Caractere inválido!"); // Se o usuário digitar um caractere diferente do esperado, será apresentado o erro e ele deverá digitar um caractere válido para encerrar o loop
 				}
 				
 			} while(desejaAlterar != 's' && desejaAlterar != 'S' && desejaAlterar != 'n' && desejaAlterar != 'N');
@@ -190,15 +187,15 @@ public class ControleClientesProdutos_Tp1 {
 		if (desejaAlterar == 'S' || desejaAlterar == 's') {
 			System.out.println("\n ~ Alterando Nome ~");
 			System.out.print("Digite o nome do(a) cliente: ");
-			nome[posicaoCliente] = ler.nextLine();
+			nome[indexCliente] = ler.nextLine();
 			
 			System.out.println("\n~ Alterando Endereço ~");
 			System.out.print("Digite o endereço do(a) cliente: ");
-			endereco[posicaoCliente] = ler.nextLine();
+			endereco[indexCliente] = ler.nextLine();
 			
 			System.out.println("\n~ Alterando Telefone ~");
 			System.out.print("Digite o telefone do(a) cliente: ");
-			telefone[posicaoCliente] = ler.nextLine();
+			telefone[indexCliente] = ler.nextLine();
 			
 			System.out.println("\nDados Atualizados!");			
 		}
@@ -207,65 +204,83 @@ public class ControleClientesProdutos_Tp1 {
 	
 	
 	
-	/*public static void cadastrarProdutos(ArrayList<String> nomeProduto, ArrayList<String> descricao, ArrayList<Double> valorCompra, ArrayList<Double> porcentLucro, ArrayList<Integer> qtdEstoque) {
+	public static void cadastrarProdutos(String nomeProduto[], String descricao[], double valorCompra[], double porcentLucro[], int qtdEstoque[]) {
 		int qtdCadastrar;
 		Scanner ler = new Scanner(System.in);
 		
 		System.out.println("\n~ CADASTRAR PRODUTOS ~");
 		
-		do { // O usuário deverá digitar uma quant. de produtos a serem cadastrados maior ou igual a 1
+		do { // O usuário deverá digitar uma quant. de produtos a serem cadastrados maior ou igual a 1 e menor que o tamanho máximo dos vetores (ver TAMANHO_VETOR)
 			System.out.print("Digite a quantidade de produtos a serem cadastrados: ");
 			qtdCadastrar = ler.nextInt();	
 			
-			if(qtdCadastrar < 1) { // Apresenta mensagem para informar o erro ao usuário
-				System.out.println("O valor inserido deve ser maior que zero!");
+			if(qtdCadastrar < 1 || qtdCadastrar > TAMANHO_VETOR) { // Apresenta mensagem para informar o erro ao usuário
+				System.out.println("O valor inserido deve ser maior que zero e menor que " + (TAMANHO_VETOR+1) + "!\n");
 			}
-		}while(qtdCadastrar < 1); //Se repete até a quant. ser maior ou igual a 1
+		}while(qtdCadastrar < 1 || qtdCadastrar > TAMANHO_VETOR); //Se repete até a quant. ser maior ou igual a 1 e menor ou igual ao TAMANHO_VETOR
 		
 		for(int aux = 0; aux < qtdCadastrar; aux++) { // Recebendo os dados dos produtos
 			ler.nextLine(); // Limpando Buffer 
 			
 			System.out.print("\nDigite o nome do " + (aux+1) + "º produto: "); // Recebendo o nome
-			nomeProduto.add(ler.nextLine());
+			nomeProduto[numProdutos] = ler.nextLine();
 			
 			System.out.print("Digite a descrição do produto: "); // Recebendo a descrição
-			descricao.add(ler.nextLine());
+			descricao[numProdutos] = ler.nextLine();
 			
 			System.out.print("Digite o valor de compra do produto: R$ "); // Recebendo o valor de compra
-			valorCompra.add(ler.nextDouble());
+			valorCompra[numProdutos] = ler.nextDouble();
 			
 			System.out.print("Digite a porcentagem de lucro do produto (sem '%'): "); // Recebendo a porcentagem de lucro
-			porcentLucro.add(ler.nextDouble());
+			porcentLucro[numProdutos] = ler.nextDouble();
 			
 			System.out.print("Digite a quantidade em estoque do produto: "); // Recebendo a qtd em estoque
-			qtdEstoque.add(ler.nextInt());
+			qtdEstoque[numProdutos] = ler.nextInt();
+			
+			numProdutos++;
 		}
 		
 	}
 	
 	
 	
-	public static void buscarProdutos(ArrayList<String> nomeProduto, ArrayList<String> descricao, ArrayList<Double> valorCompra, ArrayList<Double> porcentLucro, ArrayList<Integer> qtdEstoque) {
+	public static void buscarProdutos(String nomeProduto[], String descricao[], double valorCompra[], double porcentLucro[], int qtdEstoque[]) {
 		char desejaAlterar;
+		boolean produtoExiste = false;
 		String nomeProd;
-		int indexProduto;
+		int indexProduto = 0;
 		Scanner ler = new Scanner(System.in);
 		
 		System.out.println("\n~ BUSCAR PRODUTOS ~");
 		System.out.print("Digite o nome do(a) produto a ser procurado: ");
 		nomeProd = ler.nextLine();
 		
-		if(nomeProduto.contains(nomeProd)) {
-			System.out.println("\nDados do Produto: ");
-			System.out.println("Nome do Produto: " + nomeProduto.get(nomeProduto.indexOf(nomeProd)));
-			System.out.println("Descrição: " + descricao.get(nomeProduto.indexOf(nomeProd)));
-			System.out.println("Valor de Compra: R$ " + valorCompra.get(nomeProduto.indexOf(nomeProd)));
-			System.out.println("Porcentagem de Lucro: " + porcentLucro.get(nomeProduto.indexOf(nomeProd)) + " %");
-			System.out.println("Quantidade em Estoque: " + qtdEstoque.get(nomeProduto.indexOf(nomeProd)));
+		for(int i = 0; i < numProdutos; i++) {
+			if(nomeProduto[i].equals(nomeProd)) {
+				produtoExiste = true;
+				indexProduto = i;
+			}
+		}
+		
+		if(produtoExiste) {
+			System.out.println("\nDADOS DO PRODUTO: ");
+			System.out.println("Nome do Produto: " + nomeProduto[indexProduto]);
+			System.out.println("Descrição: " + descricao[indexProduto]);
+			System.out.println("Valor de Compra: R$ " + valorCompra[indexProduto]);
+			System.out.println("Porcentagem de Lucro: " + porcentLucro[indexProduto] + " %");
+			System.out.println("Quantidade em Estoque: " + qtdEstoque[indexProduto]);
 			
-			System.out.print("\nDeseja alterar os dados do(a) produto? ('S' ou 'N'): ");
-			desejaAlterar = ler.next().charAt(0);
-			ler.nextLine(); //Limpando Buffer
+			do { // Verificando se o usuário deseja alterar os dados do produto em questão 
+				System.out.print("\nDeseja alterar os dados do produto? ('S' ou 'N'): ");
+				desejaAlterar = ler.next().charAt(0);
+				ler.nextLine(); //Limpando Buffer
+				
+				if (desejaAlterar != 's' && desejaAlterar != 'S' && desejaAlterar != 'n' && desejaAlterar != 'N') {
+					System.out.println("Caractere inválido!"); // Se o usuário digitar um caractere diferente do esperado, será apresentado o erro e ele deverá digitar um caractere válido para encerrar o loop
+				}
+				
+			} while(desejaAlterar != 's' && desejaAlterar != 'S' && desejaAlterar != 'n' && desejaAlterar != 'N');
+			
 		} else {
 			System.out.println("\nProduto não encontrado!");
 			desejaAlterar = 'n';
@@ -274,30 +289,29 @@ public class ControleClientesProdutos_Tp1 {
 		
 		
 		if (desejaAlterar == 'S' || desejaAlterar == 's') {
-			indexProduto = nomeProduto.indexOf(nomeProd);
 			System.out.println("\n ~ Alterando Nome ~");
 			System.out.print("Digite o nome do(a) produto: ");
-			nomeProduto.set(indexProduto, ler.nextLine());
+			nomeProduto[indexProduto] = ler.nextLine();
 			
 			System.out.println("\n~ Alterando Descrição ~");
 			System.out.print("Digite a descrição do produto: ");
-			descricao.set(indexProduto, ler.nextLine());
+			descricao[indexProduto] = ler.nextLine();
 			
 			System.out.println("\n~ Alterando Valor de Compra ~");
 			System.out.print("Digite o valor de compra do produto: R$ ");
-			valorCompra.set(indexProduto, ler.nextDouble());
+			valorCompra[indexProduto] = ler.nextDouble();
 			
 			System.out.println("\n~ Alterando Porcentagem de Lucro ~");
 			System.out.print("Digite a porcentagem de lucro do produto (sem '%'): ");
-			porcentLucro.set(indexProduto, ler.nextDouble());
+			porcentLucro[indexProduto] = ler.nextDouble();
 			
 			System.out.println("\n~ Alterando Quantidade em Estoque ~");
 			System.out.print("Digite a quantidade em estoque do produto: ");
-			qtdEstoque.set(indexProduto, ler.nextInt());
+			qtdEstoque[indexProduto] = ler.nextInt();
 			
 			System.out.println("\nDados Atualizados!");			
 		}
-	}*/
+	}
 
 	
 }
